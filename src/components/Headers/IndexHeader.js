a027ec12a4bca0bf92d810bdbe33f654c06150df
DESKTOP-EPM5P8O\skyline <nipunchathuranga45@gmail.com>
2023-01-17 21:58:55 +0530

    CHG | change headers

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -1,39 +1,37 @@
 /*eslint-disable*/
-import React from "react";
+import React from 'react'
 
 // reactstrap components
-import { Container } from "reactstrap";
+import { Container } from 'reactstrap'
 // core components
 
 function IndexHeader() {
-  let pageHeader = React.createRef();
+  let pageHeader = React.createRef()
 
   React.useEffect(() => {
     if (window.innerWidth > 991) {
       const updateScroll = () => {
-        let windowScrollTop = window.pageYOffset / 3;
+        let windowScrollTop = window.pageYOffset / 3
         pageHeader.current.style.transform =
-          "translate3d(0," + windowScrollTop + "px,0)";
-      };
-      window.addEventListener("scroll", updateScroll);
+          'translate3d(0,' + windowScrollTop + 'px,0)'
+      }
+      window.addEventListener('scroll', updateScroll)
       return function cleanup() {
-        window.removeEventListener("scroll", updateScroll);
-      };
+        window.removeEventListener('scroll', updateScroll)
+      }
     }
-  });
+  })
 
   return (
-
-      <div className="page-header">
-        <div
-          className="page-header-image"
-          // style={{
-          //   backgroundImage: "url(" + require("assets/img/header.jpg") + ")",
-          // }}
-          ref={pageHeader}
-
-        ></div>
-        {/* <Container>
+    <div className="page-header">
+      <div
+        className="page-header-image"
+        // style={{
+        //   backgroundImage: "url(" + require("assets/img/header.jpg") + ")",
+        // }}
+        ref={pageHeader}
+      ></div>
+      {/* <Container>
           <div className="content-center brand">
             
           </div>
@@ -41,27 +39,29 @@ function IndexHeader() {
           </h6>
         </Container>*/}
       <Container>
-         <div class="row mt-5">
-        <div class="col-lg-6 order-1 text-lg-start">
-          <h1>
-            Work that we produce for our clients
-          </h1>
-          <p>
-            Lorem lpsum is simply dummy text of the printing and typesetting industry.
-            lorem lpsum has been the industry's standard.
+        <div class="row mt-5">
+          <div class="col-lg-6 order-1 text-lg-start">
+            <h1>Work that we produce for our clients</h1>
+            <p>
+              Lorem lpsum is simply dummy text of the printing and typesetting
+              industry. lorem lpsum has been the industry's standard.
             </p>
-          <button type="button" class="btn mb-3">Get more details</button>
-        </div>
-    
-        <div class="col-lg-6 order-2 text-lg-start">
-        <img class="fluid" src="https://img.freepik.com/free-vector/vintage-badge-hand-holding-joystick-vector-illustration-round-label-with-gamepad_74855-11224.jpg?w=740&t=st=1673200315~exp=1673200915~hmac=3a9bb4304c60fd7038e8aebfd73086ffd2a4720b14b014a703a5a75cff88eec0" 
-        alt="My Image"  />
-        </div>
+            <button type="button" class="btn mb-3">
+              Get more details
+            </button>
+          </div>
+
+          <div class="col-lg-6 order-2 text-lg-start">
+            <img
+              class="fluid"
+              src="https://img.freepik.com/free-vector/vintage-badge-hand-holding-joystick-vector-illustration-round-label-with-gamepad_74855-11224.jpg?w=740&t=st=1673200315~exp=1673200915~hmac=3a9bb4304c60fd7038e8aebfd73086ffd2a4720b14b014a703a5a75cff88eec0"
+              alt="My Image"
+            />
+          </div>
         </div>
       </Container>
-      </div>
-     
-  );
+    </div>
+  )
 }
 
-export default IndexHeader;
+export default IndexHeader
