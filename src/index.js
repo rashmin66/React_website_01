907ccde90a1044e477b73851692234aff60d9dc2
DESKTOP-EPM5P8O\skyline <nipunchathuranga45@gmail.com>
2023-01-18 10:00:39 +0530

    CHG | remove css

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 // styles for this kit
-import 'assets/css/bootstrap.min.css'
-import 'assets/scss/now-ui-kit.scss?v=1.5.0'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'assets/css/bootstrap.min.css'
+// import 'assets/scss/now-ui-kit.scss?v=1.5.0'
 
 // pages for this kit
 import Index from 'views/Index.js'
